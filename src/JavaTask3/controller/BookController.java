71ae0d9996e965f592fd60467db8b262f4868385
bernandaputri <haihaiputri@gmail.com>
2022-12-27 07:27:09 +0700

    update validasi

--- src/JavaTask3/controller/BookController.java
+++ src/JavaTask3/controller/BookController.java
@@ -15,7 +15,7 @@ public class BookController {
 
     public void showData() {
         System.out.println();
-        if (book.getTitle() == null || book.getAuthor() == null) {
+        if (bookDao.findAll().isEmpty()) {
             System.out.println("You haven't save any book, please add some!");
             System.out.println();
         } else {
@@ -25,14 +25,14 @@ public class BookController {
     }
 
     public void showById() {
-        if (book.getTitle() == null || book.getAuthor() == null) {
+        if (bookDao.findAll().isEmpty()) {
             System.out.println("You haven't save any book, please add some!");
             System.out.println();
         } else {
             System.out.println();
             System.out.print("Book ID: ");
             id = Integer.valueOf(input.nextLine());
-            
+
             if (id > bookDao.findAll().size()) {
                 System.out.println("Book with ID " + id + " doesn't exist.");
             } else {
@@ -60,37 +60,47 @@ public class BookController {
     }
 
     public void updateBook() {
-        System.out.println();
-        System.out.print("Book ID: ");
-        id = Integer.valueOf(input.nextLine());
-
-        if (id > bookDao.findAll().size()) {
-            System.out.println("Book with ID " + id + " doesn't exist.");
-        } else {
-            System.out.println(bookDao.findById(id));
-            System.out.print("Update book title: ");
-            book.setTitle(input.nextLine());
-            System.out.print("Update book author: ");
-            book.setAuthor(input.nextLine());
-            bookDao.update(book, id);
-
-            System.out.println("Book " + id + " successfully updated!");
+        if (bookDao.findAll().isEmpty()) {
+            System.out.println("You haven't save any book, please add some!");
             System.out.println();
+        } else {
+            System.out.println();
+            System.out.print("Book ID: ");
+            id = Integer.valueOf(input.nextLine());
+
+            if (id > bookDao.findAll().size()) {
+                System.out.println("Book with ID " + id + " doesn't exist.");
+            } else {
+                System.out.println(bookDao.findById(id));
+                System.out.print("Update book title: ");
+                book.setTitle(input.nextLine());
+                System.out.print("Update book author: ");
+                book.setAuthor(input.nextLine());
+                bookDao.update(book, id);
+
+                System.out.println("Book " + id + " successfully updated!");
+                System.out.println();
+            }
         }
     }
 
     public void deleteBook() {
-        System.out.println();
-        System.out.print("Book ID: ");
-        id = Integer.valueOf(input.nextLine());
-
-        if (id > bookDao.findAll().size()) {
-            System.out.println("Book with ID " + id + " doesn't exist.");
-        } else {
-            bookDao.delete(id);
-
-            System.out.println("Book " + id + " successfully deleted!");
+        if (bookDao.findAll().isEmpty()) {
+            System.out.println("You haven't save any book, please add some!");
             System.out.println();
+        } else {
+            System.out.println();
+            System.out.print("Book ID: ");
+            id = Integer.valueOf(input.nextLine());
+
+            if (id > bookDao.findAll().size()) {
+                System.out.println("Book with ID " + id + " doesn't exist.");
+            } else {
+                bookDao.delete(id);
+
+                System.out.println("Book " + id + " successfully deleted!");
+                System.out.println();
+            }
         }
     }
 }
